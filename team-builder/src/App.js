8ef5d8f1ea23ea43e3116daeda5be1a7af420611
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-19 16:18:22 -0400

    Created the team list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [teamList, setTeamList] = useState( //array of objects - each object containing a team members info
+    [
+      {
+        name: 'Jason Smith',
+        email: 'jason@jason.com',
+        role: 'HR Manager'
+      },
+
+      {
+        name: 'Rae Smith',
+        email: 'rae@rae.com',
+        role: 'Best developer in history'
+      },
+
+      {
+        name: 'Chevan',
+        email: 'chevan@chevan.com',
+        role: 'Coder & main idea guy'
+      }
+    ]
+    );
+
+    console.log(teamList);
+
   return (
     <div className="App">
       <header className="App-header">
